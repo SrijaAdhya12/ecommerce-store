12f58f3a9a7399f363cbcc497aa60718756bfff9
Srija Adhya <srijaadhya.11sc.2020@gmail.com>
2024-09-15 02:31:46 +0530

    feat: added caching

--- client/src/lib/cache.ts
+++ client/src/lib/cache.ts
@@ -1,11 +1,12 @@
 import { unstable_cache as nextCache } from "next/cache";
 
-type Callback = (...args: any[]) => Promise<any>;
+type Callback<T extends any[] = any[]> = (...args: T) => Promise<any>;
 
 export function cache<T extends Callback>(
     cb: T,
     keyParts: string[],
     options: { revalidate?: number | false; tags?: string[] } = {}
 ) {
+    // Wrap the callback with nextCache directly
     return nextCache(cb, keyParts, options);
 }
